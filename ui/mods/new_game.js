694f8d43ea518e576bddf0fc3654fa32430a48c8
gb6162166 <gb6162166@gmail.com>
2025-07-21 20:40:11 -0400

    Add files via upload

--- ui/mods/new_game.js
+++ ui/mods/new_game.js
@@ -7,4 +7,8 @@
           loc("!LOC:Red Line 2"),
           loc("!LOC:Units added or modified. Available in community mods.")
         );
+        model.localChatMessage(
+          loc("!LOC:New"),
+          loc("!LOC:AA Storm tank and Boss Pelican both provide anti-missile defence. Vanguard tank provides radar.")
+        );
       })
