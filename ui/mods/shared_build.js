4f5e850c698ba85f843fb5a2efa287d759ab8afe
gb6162166 <gb6162166@gmail.com>
2025-10-02 14:54:49 -0400

    Add files via upload

--- ui/mods/shared_build.js
+++ ui/mods/shared_build.js
@@ -2,61 +2,79 @@ var newBuild = {
 
 	"/pa/units/land/ultra_dox/ultra_dox.json": ["bot", 1001, { row: 0, column: 1 }],
 	"/pa/units/land/ultra_grenadier/ultra_grenadier.json": ["bot", 1002, { row: 0, column: 2 }],
+	"/pa/units/land/ultra_slammer/ultra_slammer.json": ["bot", 1003, { row: 0, column: 3 }],
+	"/pa/units/land/ultra_bluehawk/ultra_bluehawk.json": ["bot", 1004, { row: 0, column: 4 }],
 	"/pa/units/land/ultra_spark/ultra_spark.json": ["bot", 1005, { row: 0, column: 5 }],
 	"/pa/units/land/ultra_stinger/ultra_stinger.json": ["bot", 1006, { row: 0, column: 6 }],
-	"/pa/units/land/ultra_slammer/ultra_slammer.json": ["bot", 1013, { row: 0, column: 3 }],
-	"/pa/units/land/ultra_bluehawk/ultra_bluehawk.json": ["bot", 1014, { row: 0, column: 4 }],
+
+	"/pa/units/land/r2_fab_bot_adv/r2_fab_bot_adv.json": ["bot", 1010, { row: 1, column: 0 }],		
+	"/pa/units/land/r2_slammer/r2_slammer.json": ["bot", 1011, { row: 1, column: 1 }],		
+	"/pa/units/land/r2_gile/r2_gile.json": ["bot", 1012, { row: 1, column: 2 }],		
+	"/pa/units/land/r2_mend/r2_mend.json": ["bot", 1013, { row: 1, column: 3 }],		
+	"/pa/units/land/r2_bluehawk/r2_bluehawk.json": ["bot", 1014, { row: 1, column: 4 }],		
+	"/pa/units/land/r2_locusts/r2_locusts.json": ["bot", 1015, { row: 1, column: 5 }],		
 	"/pa/units/land/ultra_boom/ultra_boom.json": ["bot", 1016, { row: 1, column: 6 }],
 	
-	"/pa/units/land/bot_dox/bot_dox.json": ["bot", 1021, { row: 2, column: 1 }],		
+	"/pa/units/land/r2_fab_bot/r2_fab_bot.json": ["bot", 1020, { row: 2, column: 0 }],		
+	"/pa/units/land/r2_dox/r2_dox.json": ["bot", 1021, { row: 2, column: 1 }],		
 	"/pa/units/land/r2_gren/r2_gren.json": ["bot", 1022, { row: 2, column: 2 }],		
-	"/pa/units/land/bot_spark/bot_spark.json": ["bot", 1025, { row: 2, column: 5 }],		
+	"/pa/units/land/r2_stitch/r2_stitch.json": ["bot", 1023, { row: 2, column: 3 }],
+	"/pa/units/land/r2_boom/r2_boom.json": ["bot", 1024, { row: 2, column: 4 }],
+	"/pa/units/land/r2_spark/r2_spark.json": ["bot", 1025, { row: 2, column: 5 }],		
+	"/pa/units/land/r2_stinger/r2_stinger.json": ["bot", 1026, { row: 2, column: 6 }],		
 	
-	"/pa/units/land/boss_doxer/boss_doxer.json": ["boss bot", 1200, { row: 0, column: 0 }],	
-	"/pa/units/land/boss_sparkie/boss_sparkie.json": ["boss bot", 1201, { row: 1, column: 0 }],	
-
-	
-	
+	"/pa/units/land/boss_doxer/boss_doxer.json": ["boss bot", 1300, { row: 0, column: 0 }],	
+	"/pa/units/land/boss_sparkie/boss_sparkie.json": ["boss bot", 1310, { row: 1, column: 0 }],	
 	
 	"/pa/units/land/ultra_ant/ultra_ant.json": ["vehicle", 2001, { row: 0, column: 1 }],
 	"/pa/units/land/ultra_inferno/ultra_inferno.json": ["vehicle", 2002, { row: 0, column: 2 }],
+	"/pa/units/land/ultra_leveler/ultra_leveler.json": ["vehicle", 2003, { row: 0, column: 3 }],	
 	"/pa/units/land/ultra_spinner/ultra_spinner.json": ["vehicle", 2004, { row: 0, column: 4 }],
-	"/pa/units/land/ultra_drifter/ultra_drifter.json": ["vehicle", 2005, { row: 0, column: 5 }],
-	
+	"/pa/units/land/ultra_drifter/ultra_drifter.json": ["vehicle", 2005, { row: 0, column: 5 }],	
 	"/pa/units/land/land_echo/land_echo.json": ["vehicle", 2006, { row: 0, column: 6 }],
 
+	"/pa/units/land/r2_fab_veh_adv/r2_fab_veh_adv.json": ["vehicle", 2010, { row: 1, column: 0 }],		
+	"/pa/units/land/r2_storm/r2_storm.json": ["vehicle", 2014, { row: 1, column: 4 }],		
 	"/pa/units/land/aa_drifter/aa_drifter.json": ["vehicle", 2015, { row: 1, column: 5 }],
-	"/pa/units/land/ultra_leveler/ultra_leveler.json": ["vehicle", 2103, { row: 0, column: 3 }],
+	
+	"/pa/units/land/r2_fab_veh/r2_fab_veh.json": ["vehicle", 2020, { row: 2, column: 0 }],		
+	"/pa/units/land/r2_ant/r2_ant.json": ["vehicle", 2021, { row: 2, column: 1 }],	
+	"/pa/units/land/r2_inferno/r2_inferno.json": ["vehicle", 2022, { row: 2, column: 2 }],	
+	"/pa/units/land/r2_skitter/r2_skitter.json": ["vehicle", 2023, { row: 2, column: 3 }],
+	"/pa/units/land/r2_drifter/r2_drifter.json": ["vehicle", 2025, { row: 2, column: 5 }],	
+	
+	"/pa/units/land/boss_drifter/boss_drifter.json": ["boss vehicle", 2310, { row: 1, column: 0 }],	
+	"/pa/units/land/boss_stryker/boss_stryker.json": ["boss vehicle", 2320, { row: 2, column: 0 }],	
 	
 	
-	"/pa/units/land/boss_drifter/boss_drifter.json": ["boss vehicle", 2210, { row: 1, column: 0 }],	
-	"/pa/units/land/boss_stryker/boss_stryker.json": ["boss vehicle", 2220, { row: 2, column: 0 }],	
+	"/pa/units/air/r2_angel/r2_angel.json": ["air", 3000, { row: 0, column: 0 }],
+	"/pa/units/air/ultra_phoenix/ultra_phoenix.json": ["air", 3001, { row: 0, column: 1 }],
+	"/pa/units/air/ultra_kestrel/ultra_kestrel.json": ["air", 3002, { row: 0, column: 2 }],
+	"/pa/units/air/ultra_horsefly/ultra_horsefly.json": ["air", 3003, { row: 0, column: 3 }],
 	
+	"/pa/units/air/r2_horsefly/r2_horsefly.json": ["air", 3003, { row: 0, column: 7 }],
 	
-	"/pa/units/air/r2_angel/r2_angel.json": ["air", 3100, { row: 0, column: 0 }],
-	"/pa/units/air/ultra_phoenix/ultra_phoenix.json": ["air", 3101, { row: 0, column: 1 }],
-	"/pa/units/air/ultra_kestrel/ultra_kestrel.json": ["air", 3102, { row: 0, column: 2 }],
-	"/pa/units/air/ultra_horsefly/ultra_horsefly.json": ["air", 3103, { row: 0, column: 3 }],
-	"/pa/units/air/ultra_pelican/ultra_pelican.json": ["air", 3104, { row: 0, column: 4 }],	
-	"/pa/units/air/ultra_icarus/ultra_icarus.json": ["air", 3105, { row: 0, column: 5 }],		
+	"/pa/units/air/ultra_pelican/ultra_pelican.json": ["air", 3004, { row: 0, column: 4 }],	
+	"/pa/units/air/ultra_icarus/ultra_icarus.json": ["air", 3005, { row: 0, column: 5 }],		
 	
-	"/pa/units/air/boss_kestrel/boss_kestrel.json": ["boss air", 3201, { row: 0, column: 0 }],
-	"/pa/units/air/boss_icarus/boss_icarus.json": ["boss air", 3202, { row: 1, column: 0 }],
-	"/pa/units/air/boss_pelican/boss_pelican.json": ["boss air", 3203, { row: 2, column: 0 }],
+	"/pa/units/air/boss_kestrel/boss_kestrel.json": ["boss air", 3300, { row: 0, column: 0 }],
+	"/pa/units/air/boss_icarus/boss_icarus.json": ["boss air", 3310, { row: 1, column: 0 }],
+	"/pa/units/air/boss_pelican/boss_pelican.json": ["boss air", 3320, { row: 2, column: 0 }],
 	
 	
 	"/pa/units/sea/swordfish/swordfish.json": ["sea", 4001, { row: 0, column: 1 }],
 	"/pa/units/sea/ultra_orca/ultra_orca.json": ["sea", 4002, { row: 0, column: 2 }],
 	"/pa/units/sea/ultra_piranha/ultra_piranha.json": ["sea", 4004, { row: 0, column: 4 }],
 	"/pa/units/sea/archerfish/archerfish.json": ["sea", 4005, { row: 0, column: 5 }],
+	"/pa/units/sea/megalodon/megalodon.json": ["sea", 4015, { row: 1, column: 5 }],
 	
-	"/pa/units/sea/megalodon/megalodon.json": ["sea", 4105, { row: 1, column: 5 }],
+	"/pa/units/land/r2_zapper_adv/r2_zapper_adv.json": ["combat", 5000, { row: 0, column: 0 }],
+	"/pa/units/land/r2_lob_gren/r2_lob_gren.json": ["combat", 5005, { row: 0, column: 5 }],
+	"/pa/units/land/r2_zapper_twin/r2_zapper_twin.json": ["combat", 5010, { row: 1, column: 0 }],
+	"/pa/units/land/r2_lob_dox/r2_lob_dox.json": ["combat", 5012, { row: 1, column: 2 }],
+	"/pa/units/land/r2_lob_boom/r2_lob_boom.json": ["combat", 5015, { row: 1, column: 5 }],
+	"/pa/units/land/r2_zapper/r2_zapper.json": ["combat", 5020, { row: 2, column: 0 }]
 	
-	
-	"/pa/units/land/artillery_spider_launcher/artillery_spider_launcher.json": ["combat", 5105, { row: 1, column: 5 }],
-	"/pa/units/land/artillery_grenadier_launcher/artillery_grenadier_launcher.json": ["combat", 5007, { row: 0, column: 5 }]
-	
-
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
     _.extend(Build.HotkeyModel.SpecIdToGridMap, newBuild);
